27bfdb397e15ee1bec35fd72dc7672e77511f4e3
Keyla Vanessa Alba Molina <89478438+Kalbam@users.noreply.github.com>
2025-06-24 03:33:31 -0300

    Update load_data_to_postgres.py

--- load_data_to_postgres.py
+++ load_data_to_postgres.py
@@ -30,17 +30,17 @@ db_host = "dpg-coq0ptf109ks739mecfg-a.oregon-postgres.render.com"
 db_port = "5432"
 db_name = "radiation_inmet"
 
-# Crear motor de conexión
 engine = create_engine(
     f"postgresql+psycopg2://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
 )
 
-# Cargar archivos CSV locales (deben estar en el mismo directorio)
-df_ban = pd.read_csv("df_Ban_Inmet.csv", encoding="utf-8-sig")
-df_hist = pd.read_csv("df_Hist_Inmet.csv", encoding="utf-8-sig")
+try:
+    df_ban = pd.read_csv("df_Ban_Inmet.csv", encoding="utf-8-sig")
+    df_hist = pd.read_csv("df_Hist_Inmet.csv", encoding="utf-8-sig")
 
-# Subir a PostgreSQL (Render)
-df_ban.to_sql("df_ban_inmet", engine, if_exists="replace", index=False)
-df_hist.to_sql("df_hist_inmet", engine, if_exists="replace", index=False)
+    df_ban.to_sql("df_ban_inmet", engine, if_exists="replace", index=False)
+    df_hist.to_sql("df_hist_inmet", engine, if_exists="replace", index=False)
 
-print(" Datos cargados con éxito en PostgreSQL Render.")
+    print(" Datos cargados con éxito en PostgreSQL Render.")
+except FileNotFoundError as e:
+    print(f" Archivo no encontrado: {e.filename}")
