b249c28c0d1529bf3a3f521e32911d9d7a3525f2
Keyla Vanessa Alba Molina <89478438+Kalbam@users.noreply.github.com>
2025-06-24 12:08:55 -0300

    Update load_data_to_postgres.py

--- load_data_to_postgres.py
+++ load_data_to_postgres.py
@@ -1,5 +1,5 @@
-# import pandas as pd
-# from sqlalchemy import create_engine
+import pandas as pd
+from sqlalchemy import create_engine
 
 
 # db_user = "postgres"
@@ -8,28 +8,28 @@
 # db_port = "5432"
 # db_name = "radiation_inmet"
 
-
 # engine = create_engine(f"postgresql+psycopg2://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}")
-
 
 # df_ban = pd.read_csv("df_Ban_Inmet.csv")
 # df_hist = pd.read_csv("df_Hist_Inmet.csv")
 
-
 # df_ban.to_sql("df_ban_inmet", engine, if_exists="replace", index=False)
 # df_hist.to_sql("df_hist_inmet", engine, if_exists="replace", index=False)
 
+
+
 # print(" Datos cargados con éxito en PostgreSQL.")
-db_user = "radiation_inmet_db_v1_user"
-db_password = "5EshSNxUVp7iJIEX8ZLFA7miiGgcQeOJ"
-db_host = "dpg-d1d2ptqdbo4c73cb2j00-a.oregon-postgres.render.com"
-db_port = "5432"
-db_name = "radiation_inmet_db_v1"
+# db_user = "radiation_inmet_db_v1_user"
+# db_password = "5EshSNxUVp7iJIEX8ZLFA7miiGgcQeOJ"
+# db_host = "dpg-d1d2ptqdbo4c73cb2j00-a.oregon-postgres.render.com"
+# db_port = "5432"
+# db_name = "radiation_inmet_db_v1"
 
 # Crear motor de conexión a Render
-engine = create_engine(
-    f"postgresql+psycopg2://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}?sslmode=require"
-)
+db_url = "postgresql://radiation_inmet_db_v1_user:5EshSNxUVp7iJIEX8ZLFA7miiGgcQeOJ@dpg-d1d2ptqdbo4c73cb2j00-a:5432/radiation_inmet_db_v1"
+
+# Crear el engine
+engine = create_engine(db_url)
 
 try:
     # Leer archivos locales (asegúrate que estén en la misma carpeta cuando ejecutes esto localmente)
