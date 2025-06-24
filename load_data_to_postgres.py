ed941d97275dcfc0204bdf0592025beb242c0afd
Keyla Vanessa Alba Molina <89478438+Kalbam@users.noreply.github.com>
2025-06-24 03:40:07 -0300

    Update load_data_to_postgres.py

--- load_data_to_postgres.py
+++ load_data_to_postgres.py
@@ -20,27 +20,30 @@
 # df_hist.to_sql("df_hist_inmet", engine, if_exists="replace", index=False)
 
 # print(" Datos cargados con éxito en PostgreSQL.")
-import pandas as pd
-from sqlalchemy import create_engine
-
-# Credenciales de tu base de datos en Render
 db_user = "postgres"
 db_password = "KeylaAlba572"
-db_host = "dpg-coq0ptf109ks739mecfg-a.oregon-postgres.render.com"
+db_host = "dpg-d1d2ptqdbo4c73cb2j00-a.oregon-postgres.render.com"
 db_port = "5432"
 db_name = "radiation_inmet"
 
+# Crear motor de conexión a Render
 engine = create_engine(
     f"postgresql+psycopg2://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
 )
 
 try:
+    # Leer CSV desde la carpeta local
     df_ban = pd.read_csv("df_Ban_Inmet.csv", encoding="utf-8-sig")
     df_hist = pd.read_csv("df_Hist_Inmet.csv", encoding="utf-8-sig")
 
+    # Subir a PostgreSQL (Render)
     df_ban.to_sql("df_ban_inmet", engine, if_exists="replace", index=False)
     df_hist.to_sql("df_hist_inmet", engine, if_exists="replace", index=False)
 
     print(" Datos cargados con éxito en PostgreSQL Render.")
+
 except FileNotFoundError as e:
     print(f" Archivo no encontrado: {e.filename}")
+
+except Exception as e:
+    print(f" Error inesperado: {e}")
