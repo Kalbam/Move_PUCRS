e395c57ac4508e8fc102d24a042729d85f998c09
Keyla Vanessa Alba Molina <89478438+Kalbam@users.noreply.github.com>
2025-07-09 07:18:39 -0300

    Update app_project.py

--- app_project.py
+++ app_project.py
@@ -1262,17 +1262,7 @@ app.layout = dbc.Container(
     fluid=True
 )
 
-app.layout = html.Div([
-    html.Script(src="https://polyfill.io/v3/polyfill.min.js?features=es6"),
-    html.Script(
-        src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js",
-        id="MathJax-script",
-        **{"async": "async"}
-    ),
 
-    html.H1("Metodologia", style={'textAlign': 'center', 'color': '#003366'}),
-    subtabs_metodologia
-])
 
 # ═══════════════════════════════════════════════════════════════════════
 # CALLBACK  • limpia, agrupa y dibuja
